990c021800c7b875c937042ad98f96cd83d1c568
Mahaboobsab <git config --global user.email YourEmailProvidedToGit@Somedomain.com>
2025-07-10 21:31:40 +0530

    Initial Commit

---
+++ TCA-Architecture/TCA_ArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_ArchitectureApp.swift
+//  TCA-Architecture
+//
+//  Created by Alkit Gupta on 10/07/25.
+//
+
+import SwiftUI
+
+@main
+struct TCA_ArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
